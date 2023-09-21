d6510df2ea4f36b76d9cc76c6229cb1d78021789
robert1015 <zzh101596@gmail.com>
2023-09-21 17:32:36 +0900

    Step 2 and Step 3

--- Market.java
+++ Market.java
@@ -4,9 +4,9 @@ public enum Market {
     PRIME, GROWTH, STANDARD;
     static Market parseMarket(String input) throws InvalidPropertiesFormatException{
         switch (input) {
-            case "PRIME": return Market.PRIME;
-            case "GROWTH": return Market.GROWTH;
-            case "STANDARD": return Market.STANDARD;
+            case "PRIME", "prime": return Market.PRIME;
+            case "GROWTH", "growth": return Market.GROWTH;
+            case "STANDARD", "standard": return Market.STANDARD;
             default: throw new InvalidPropertiesFormatException("");
         }
     }
