83c164636ef5e47ba3c2d71ef5c110e582a819a3
robert1015 <zzh101596@gmail.com>
2023-09-22 12:17:16 +0900

    Step 2 and Step 3

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 import java.text.DecimalFormat;
 import java.util.regex.Matcher;
@@ -73,24 +75,24 @@ public class Main {
         System.out.println("アプリケーションを終了します。");
     }
     static void ShowAllStocks(StockListManager stockListManager) {
-        if(stockListManager.getStocks().size() == 0) {
+        if (stockListManager.getStocks().size() == 0) {
             System.out.println("ERROR: 登録された銘柄マスタはありません。");
             return;
         }
-        System.out.println("|" + "=".repeat(61) + "|" );
+        System.out.println("|" + "=".repeat(61) + "|");
         // 4+2 25+2 8+2 15+2
         System.out.println("| Code | Product Name              | Market   | Shares Issued |");
         System.out.println("|------+---------------------------+----------+---------------|");
-        for(Stock stock: stockListManager.getStocks()) { //逐行读数据
+        for (Stock stock : stockListManager.getStocks()) { //逐行读数据
             System.out.print("| " + stock.getCode() + " ");
-            if(stock.getProductName().length() > 22)
-                System.out.print("| " + stock.getProductName().substring(0,22) + "... ");
+            if (stock.getProductName().length() > 22)
+                System.out.print("| " + stock.getProductName().substring(0, 22) + "... ");
             else
-                System.out.print("| " + stock.getProductName() + " ".repeat(26-stock.getProductName().length()));
+                System.out.print("| " + stock.getProductName() + " ".repeat(26 - stock.getProductName().length()));
             DecimalFormat df = new DecimalFormat("#,###");
             System.out.printf("| %-8s | %13s |\n", stock.getMarket(), df.format(stock.getSharesIssued()));
         }
-        System.out.println("|" + "=".repeat(61) + "|" );
+        System.out.println("|" + "=".repeat(61) + "|");
     }
 
 
@@ -100,32 +102,32 @@ public class Main {
 
         //输入名字
         ProductName productName;
-        while(true) {
-            System.out.print("銘柄名> ");
+        while (true) {
+            System.out.print("銘柄名「a-z/A-Z/0-9/./,/非連続スペース」> ");
             String input = sc.nextLine();
-            if(input.equals("exit")) //输入exit退出
+            if (input.equals("exit")) //输入exit退出
                 return;
             try {
                 productName = new ProductName(input);
-                if(stockManager.containsStockByName(productName)) {
+                if (stockManager.containsStockByName(productName)) {
                     System.out.println("ERROR: この銘柄名はすでに存在している。");
                 } else break;
-            } catch (Exception e) {
-                System.out.println("不正な銘柄名の入力。「a-z, A-Z, 0-9, .と非連続スペース」");
+            } catch (InvalidPropertiesFormatException e) {
+                System.out.println("不正な銘柄名の入力。「a-z/A-Z/0-9/./,/非連続スペース」");
             }
         }
 
         //输入code
         String code;
-        while(true){
-            System.out.print("銘柄コード> ");
+        while (true) {
+            System.out.print("銘柄コード「4桁の半角数字」> ");
             code = sc.nextLine();
-            if(code.equals("exit"))
+            if (code.equals("exit"))
                 return;
             Pattern pattern = Pattern.compile("^[0-9]{4}$");
             Matcher matcher = pattern.matcher(code);
             if(matcher.matches()) {
-                if(stockManager.containsStockByCode(code)){
+                if (stockManager.containsStockByCode(code)) {
                     System.out.println("ERROR: この銘柄コードはすでに存在している。");
                 } else break;
             } else {
@@ -135,32 +137,32 @@ public class Main {
 
         //输入market
         Market market;
-        while(true){
-            System.out.print("上場市場> ");
+        while (true) {
+            System.out.print("上場市場「PRIME、STANDARD、GROWTH」> ");
             String input = sc.nextLine();
-            if(input.equals("exit"))
+            if (input.equals("exit"))
                 return;
             try {
                 market = Market.parseMarket(input);
                 break;
-            }catch (Exception e) {
+            } catch (InvalidPropertiesFormatException e) {
                 System.out.println("ERROR: 上場市場はPRIME、STANDARD、GROWTHのいずれかを入力してください。");
             }
         }
 
         //输入shares
         long sharesIssued;
-        while(true) {
-            System.out.print("発行済み株式数(0-2,000,000,000)> ");
+        while (true) {
+            System.out.print("発行済み株式数「0-9,223,372,036,854,775,807」> "); //long数据类型的最大值
             String input = sc.nextLine();
             if (input.equals("exit"))
                 return;
-            if(input.length() > 10) {
+            if (input.length() > 10) {
                 System.out.println("範囲内の正の整数を入力してください。");
             } else {
                 try {
                     sharesIssued = Long.parseUnsignedLong(input);
-                    if(sharesIssued < 0 || sharesIssued > 2000000000) {
+                    if (sharesIssued < 0) {
                         System.out.println("範囲内の正の整数を入力してください。");
                     } else break;
                 } catch (NumberFormatException e) {
@@ -169,36 +171,36 @@ public class Main {
             }
         }
         stockManager.AddStock(new Stock(code, productName, market, sharesIssued));
-        System.out.println(productName + "を新規銘柄として登録しました");
+        System.out.println(productName.getValue() + "を新規銘柄として登録しました");
     }
+
 
     static void AddNewTransAction(StockListManager stockManager, StockTradeManager transactionManager) {
         Scanner sc = new Scanner(System.in);
         System.out.println("新規取引を登録します");
         LocalDateTime time;
         while(true) {
-            System.out.print("取引日時（yyyy-MM-dd HH:mm）> ");
-            String timeStr = sc.nextLine();
-            if(timeStr.equals("exit"))
+            System.out.print("取引日時「yyyy-MM-dd HH:mm」> ");
+            String input = sc.nextLine();
+            if(input.equals("exit"))
                 return;
             try {
-                time = LocalDateTime.parse(timeStr, StockTradeManager.formatter);
+                time = LocalDateTime.parse(input, StockTradeManager.formatter);
                 if(time.isAfter(LocalDateTime.now())) {
                     System.out.println("ERROR: 取引日時の入力値は現在時間よりも過去の日時である必要がある。");
-                }else if(!(time.getDayOfWeek().getValue()>=1 &&
-                        time.getDayOfWeek().getValue()<=5 &&
-                        time.getHour()>=9 &&
-                        time.getHour()<15)) {
+                }else if(time.getDayOfWeek().getValue()>=6 ||
+                time.toLocalTime().isBefore(LocalTime.parse("09:00:00")) ||
+                time.toLocalTime().isAfter(LocalTime.parse("15:00:00"))) {
                     System.out.println("ERROR: 取引日時は平日 (月曜〜金曜) の 9:00〜15:00 の間に収まるもののみを受け付けてください");
                 } else break;
-            } catch (Exception e) {
-                System.out.println("ERROR: 取引日時の形式は\"yyyy-MM-dd HH:mm\"");
+            } catch (DateTimeParseException e) {
+                System.out.println("ERROR: 取引日時の形式は「yyyy-MM-dd HH:mm」");
             }
         }
 
        ProductName productName;
         while(true) {
-            System.out.print("銘柄名> ");
+            System.out.print("銘柄名「a-z/A-Z/0-9/./,/非連続スペース」> ");
             String input = sc.nextLine();
             if(input.equals("exit"))
                 return;
@@ -207,8 +209,8 @@ public class Main {
                 if(!stockManager.containsStockByName(productName)) {
                     System.out.println("ERROR: 入力された銘柄名は登録されてない。");
                 } else break;
-            } catch (Exception e) {
-               System.out.println("不正な銘柄名の入力。「a-z, A-Z, 0-9, .と非連続スペース」");
+            } catch (InvalidPropertiesFormatException e) {
+               System.out.println("不正な銘柄名の入力。「a-z/A-Z/0-9/./,/非連続スペース」");
             }
         }
         String code = stockManager.getByName(productName).getCode();
@@ -216,13 +218,13 @@ public class Main {
         boolean tradeType;
         while(true) {
             System.out.print("売買区分「買/売」> ");
-            String tradeTypeStr = sc.nextLine();
-            if(tradeTypeStr.equals("exit"))
+            String input = sc.nextLine();
+            if(input.equals("exit"))
                 return;
-            if(tradeTypeStr.equals("売")) {
+            if(input.equals("売")) {
                 tradeType = false;
                 break;
-            } else if(tradeTypeStr.equals("買")) {
+            } else if(input.equals("買")) {
                 tradeType = true;
                 break;
             }else {
@@ -232,7 +234,7 @@ public class Main {
 
         int amount;
         while (true) {
-            System.out.print("数量> ");
+            System.out.print("数量「100株単位, 上限は223,372,036,854,775,800」> ");
             String input = sc.nextLine();
             if (input.equals("exit"))
                 return;
@@ -241,32 +243,37 @@ public class Main {
                 if(amount % 100 != 0) {
                     System.out.println("ERROR: 取引数量は100株単位で扱わなければならない。");
                 } else if(amount <= 0) { //交易数量是个正数
-                    System.out.println("ERROR: 取引数量は正数で入力してください。");
+                    System.out.println("ERROR: 取引数量は正整数で入力してください。");
                 } else {
                     break;
                 }
             } catch (NumberFormatException e) {
-                System.out.println("ERROR: 取引数量は整数で入力してください。");
+                System.out.println("ERROR: 取引数量は正整数で入力してください。");
             }
         }
 
         BigDecimal pricePerShare;
         while (true) {
-            System.out.print("取引単価> ");
+            System.out.print("取引単価「小数点以下第2位まで扱える」> ");
             String input = sc.nextLine();
             if (input.equals("exit"))
                 return;
             try {
                 double price = Double.parseDouble(input);
+                String[] numStr = input.split("\\.");
+                if(numStr.length == 2 && numStr[1].length() > 2) {
+                    System.out.println("ERROR: 取引単価は小数点以下2位まで入力してください。");
+                    continue;
+                }
                 if(price <= 0) { //判断交易单价是个正数
                     System.out.println("ERROR: 取引単価は正数で入力してください。");
                 } else {
                     pricePerShare = BigDecimal.valueOf(price);
-                    pricePerShare = pricePerShare.setScale(2, RoundingMode.HALF_UP);
+                    pricePerShare = pricePerShare.setScale(2, RoundingMode.HALF_UP); //保留前两位小数
                     break;
                 }
             } catch (NumberFormatException e) {
-                System.out.println("ERROR: 取引単価は数字で入力してください。");
+                System.out.println("ERROR: 取引単価は正数で入力してください。");
             }
         }
         Trade log = new Trade(time, productName, code, tradeType, amount, pricePerShare);
